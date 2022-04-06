37465314b22add73f862305efb1297a1315ea412
Huzihua <2567712208@qq.com>
2022-04-06 14:49:31 +0800

    backend 2.5  fix: start.sh for linux

--- backend/start.sh
+++ backend/start.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-java -jar /target/backend-0.0.1-SNAPSHOT.jar --spring.prfiles.active=prod
+java -jar homework/cBPM-blockchain-system/backend/target/backend-0.0.1-SNAPSHOT.jar --spring.prfiles.active=prod
