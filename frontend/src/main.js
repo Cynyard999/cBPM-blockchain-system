d3aa06958d51d1dc469f77033aa715c5de7b9243
Huzihua <2567712208@qq.com>
2022-04-06 23:10:48 +0800

    fix:main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,11 @@
 import {createApp} from 'vue'
 import {createPinia} from 'pinia'
+import axios from 'axios'
+import VueAxios from "vue-axios";
 import App from './App.vue'
 import router from "./router";
 
-createApp(App).use(createPinia()).use(router).mount('#app');
+createApp(App).use(createPinia().use(axios).use(VueAxios).use(router)).mount('#app')
 
-new Vue({
-    el: '#app',
-    router,
-    components: {App},
-    template: '<App/>'
-});
+
 
