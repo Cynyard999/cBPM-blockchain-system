7e107fb5a7fe34fdccff1a1d2e0d9d35666d4c47
Cynyard999 <cynyardqiu@outlook.com>
2022-04-07 09:40:35 +0800

    fix: log generator

--- backend/src/main/java/com/cbpm/backend/util/LogWriter.java
+++ backend/src/main/java/com/cbpm/backend/util/LogWriter.java
@@ -5,15 +5,16 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * @author Polaris
- * @version 1.0
- * @description: queryLog
- * @date 2022/4/6 15:50
+ * @author Polaris, cynyard
+ * @version 1.1
+ * @date 2022/4/7 9:50
  */
 @Slf4j
 @Configuration
@@ -25,20 +26,21 @@ public class LogWriter {
     private String logPackagePath;
 
 
-    public void writeLog(String logStr)  {
+    public void writeLog(String logStr) {
         try {
-            System.out.println("logPath:"+this.logPackagePath);
-            File file = new File(this.logPackagePath+"log.txt");
+            Long timeStamp = System.currentTimeMillis();
+            String date = new SimpleDateFormat("yyyy-MM-dd")
+                    .format(new Date(Long.parseLong(String.valueOf(timeStamp))));
+            File file = new File(this.logPackagePath + date + ".log");
             if (!file.exists()) {
                 file.createNewFile();
             }
-            FileWriter fileWritter = new FileWriter(file, true);
+            FileWriter fileWriter = new FileWriter(file, true);
             Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-            System.out.println(timestamp);
-            fileWritter.write(timestamp+":"+"\n"+logStr+"\n");
-            fileWritter.close();
-        }catch (IOException e){
-            System.out.println("write to log.txt wrong.");
+            fileWriter.write(timestamp.toString() + ": " + logStr + "\n");
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("writing to log failed.");
         }
 
     }
