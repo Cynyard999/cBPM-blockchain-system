def9b84a8c796dcf00b85ca8ac2ae32f35cc2669
Cynyard999 <cynyardqiu@outlook.com>
2022-04-10 01:30:36 +0800

    feat: router based on user info

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
+import {ElMessage} from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const routerHistory = createWebHistory();
 
@@ -15,21 +17,77 @@ const router = createRouter({
         },
         {
             path: '/manufacturer',
-            component: () => import('../views/Manufacturer.vue')
+            component: () => import('../views/Manufacturer.vue'),
+            beforeEnter: (to, from, next) => {
+                let userInfo = JSON.parse(window.localStorage.getItem("user"));
+                if (userInfo['orgType'] !== 'manufacturer') {
+                    ElMessage({
+                        message: '企业信息不匹配',
+                        type: 'warning',
+                    });
+                    next('/home');
+                }
+                next()
+            },
         },
         {
             path: '/carrier',
-            component: () => import('../views/Carrier.vue')
+            component: () => import('../views/Carrier.vue'),
+            beforeEnter: (to, from, next) => {
+                let userInfo = JSON.parse(window.localStorage.getItem("user"));
+                if (userInfo['orgType'] !== 'carrier') {
+                    ElMessage({
+                        message: '企业信息不匹配',
+                        type: 'warning',
+                    });
+                    next('/home');
+                }
+                next()
+            },
         },
         {
             path: '/supplier',
-            component: () => import('../views/Supplier.vue')
+            component: () => import('../views/Supplier.vue'),
+            beforeEnter: (to, from, next) => {
+                let userInfo = JSON.parse(window.localStorage.getItem("user"));
+                if (userInfo['orgType'] !== 'supplier') {
+                    ElMessage({
+                        message: '企业信息不匹配',
+                        type: 'warning',
+                    });
+                    next('/home');
+                }
+                next()
+            },
         },
         {
             path: '/middleman',
-            component: () => import('../views/Middleman.vue')
+            component: () => import('../views/Middleman.vue'),
+            beforeEnter: (to, from, next) => {
+                let userInfo = JSON.parse(window.localStorage.getItem("user"));
+                if (userInfo['orgType'] !== 'middleman') {
+                    ElMessage({
+                        message: '企业信息不匹配',
+                        type: 'warning',
+                    });
+                    next('/home');
+                }
+                next()
+            },
         }
     ]
 });
 
+router.beforeEach((to, from, next) => {
+    if (window.localStorage.getItem("user") === null || window.localStorage.getItem("user") === undefined) {
+        ElMessage({
+            message: '请先登录',
+            type: 'warning',
+        });
+        next('/home');
+    }
+    else{
+        next();
+    }
+});
 export default router
