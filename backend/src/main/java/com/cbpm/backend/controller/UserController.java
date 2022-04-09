c632b878c2e65c8b2691dafedc92aee215fcb27d
Huzihua <2567712208@qq.com>
2022-04-09 15:31:27 +0800

    3.1 fix:login return token

--- backend/src/main/java/com/cbpm/backend/controller/UserController.java
+++ backend/src/main/java/com/cbpm/backend/controller/UserController.java
@@ -11,6 +11,8 @@ import com.cbpm.backend.serviceImpl.ApiImpl;
 import com.cbpm.backend.serviceImpl.UserImpl;
 import com.cbpm.backend.util.LogWriter;
 import com.cbpm.backend.vo.ResponseVo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import com.alibaba.fastjson.JSONObject;
@@ -27,16 +29,25 @@ public class UserController {
 
     @PostMapping("/register")
     ResponseVo register(@RequestBody JSONObject jsonObject) {
-        ResponseVo responseVo=userImpl.register(jsonObject);
-        logWriter.writeLog("Request: "+jsonObject.toJSONString()+"\n"+"Response: "+responseVo.toString());
+        ResponseVo responseVo = userImpl.register(jsonObject);
+        logWriter.writeLog("Request: " + jsonObject.toJSONString() + "\n" + "Response: "
+                + responseVo.toString());
         return responseVo;
 
     }
 
     @PostMapping("/login")
-    ResponseVo login(@RequestBody JSONObject jsonObject) {
-        ResponseVo responseVo=userImpl.login(jsonObject);
-        logWriter.writeLog("Request: "+jsonObject.toJSONString()+"\n"+"Response: "+responseVo.toString());
-        return responseVo;
+    ResponseEntity<String> login(@RequestBody JSONObject jsonObject) {
+        ResponseVo responseVo = userImpl.login(jsonObject);
+        logWriter.writeLog("Request: " + jsonObject.toJSONString() + "\n" + "Response: "
+                + responseVo.toString());
+        HttpHeaders responseHeaders = new HttpHeaders();
+        if (responseVo.isSuccess()) {
+            responseHeaders.set("Authorization", responseVo.getMessage());
+
+            return ResponseEntity.ok().headers(responseHeaders).body("login success");
+        } else {
+            return ResponseEntity.status(401).body(responseVo.getMessage());
+        }
     }
 }
